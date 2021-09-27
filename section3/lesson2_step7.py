0d8b3e79c213a423e2d5714e53e7d1d6bc4fecb8
Zhannur_tester <zhannur.akhmetkhanov@samgau.com>
2021-09-27 13:00:12 +0600

    create section3, add lesson2_step7

---
+++ section3/lesson2_step7.py
@@ -0,0 +1,13 @@
+actual_result = "abrakadabra"
+print("Wrong text, got " + actual_result + ", something wrong")
+
+print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
+print("---------------------------------------------------------------------------------------")
+
+str1 = "one"
+str2 = "two"
+str3 = "three"
+
+print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+actual_result = "abrakadabra"
+print(f"Wrong text, got {actual_result}, something wrong")
